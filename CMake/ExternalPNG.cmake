ffafffe5ee86760429242461646054f2759d35fd
rozenshtein <rozenshtein@me.com>
2014-04-20 17:36:02 +0400

    Fixed broken mirror, causing #388 issue

--- CMake/ExternalPNG.cmake
+++ CMake/ExternalPNG.cmake
@@ -4,7 +4,7 @@ INCLUDE(ExternalProject)
 SET(libpng_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/libpng)
 
 SET(libpng_CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> 
+    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 	-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
     -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
@@ -41,7 +41,7 @@ EXTERNALPROJECT_ADD(libpng
     PREFIX ${libpng_PREFIX}
 
     DOWNLOAD_DIR ${POLYCODE_DEPS_DOWNLOAD_DIR}
-    URL ftp://ftp.simplesystems.org/pub/libpng/png/src/history/libpng15/libpng-1.5.10.tar.gz
+    URL http://cznic.dl.sourceforge.net/project/libpng/libpng15/older-releases/1.5.10/libpng-1.5.10.tar.gz
     URL_MD5 9e5d864bce8f06751bbd99962ecf4aad
 
     INSTALL_DIR ${POLYCODE_DEPS_CORE_PREFIX}
